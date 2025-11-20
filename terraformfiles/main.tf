3491813364ef91e49fa1f74e4af47387a4a2946f
Santhosh Kumar Y <santhoshkumarymail@gmail.com>
2025-11-20 21:34:45 +0530

    Fix formatting of AMI string in main.tf

--- terraformfiles/main.tf
+++ terraformfiles/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "test-server" {
-  ami = "ami-0fa3fe0fa7920f68e
+  ami = "ami-0fa3fe0fa7920f68e"
   instance_type = "t3.small"
   key_name = "bookmyshow"
   vpc_security_group_ids = ["sg-038934792fd6e7c3f"]
